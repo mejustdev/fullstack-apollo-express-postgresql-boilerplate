1de1817ac859b4b04b98da16df3f13286abcf94a
20twa07 <48910851+20twa07@users.noreply.github.com>
2020-07-02 01:33:37 +0200

    initialize server

---
+++ server/src/index.js
@@ -0,0 +1,34 @@
+const { ApolloServer, gql } = require('apollo-server-express');
+const express = require('express');
+
+const app = express();
+
+const schema = gql`
+  type Query {
+    me: User
+  }
+  type User {
+    username: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    me: () => {
+      return {
+        username: 'Mehmet',
+      };
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 8000 }, () => {
+  console.log('Apollo Server on http://localhost:8000/graphql');
+});
