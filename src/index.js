c11897252c839c662fe8d2fa2269c83973275965
20twa07 <48910851+20twa07@users.noreply.github.com>
2020-08-18 15:37:57 +0200

    add GraphQL Schema Stitching -Technical Separation

--- src/index.js
+++ src/index.js
@@ -1,135 +1,22 @@
-const { ApolloServer, gql } = require('apollo-server-express');
+const { ApolloServer } = require('apollo-server-express');
 import cors from 'cors';
 import 'dotenv/config';
-import { v4 as uuidv4 } from 'uuid';
+
+import schema from './schema';
+import resolvers from './resolvers';
+import models from './models';
+
 const express = require('express');
 
 const app = express();
 app.use(cors());
 
-let users = {
-  1: {
-    id: '1',
-    username: 'Mehmet E.',
-    messageIds: [1],
-  },
-  2: {
-    id: '2',
-    username: 'Dave Davids',
-    messageIds: [2],
-  },
-};
-
-const me = users[1];
-
-let messages = {
-  1: {
-    id: '1',
-    text: 'Hello World',
-    userId: '1',
-  },
-  2: {
-    id: '2',
-    text: 'By World',
-    userId: '2',
-  },
-};
-
-const schema = gql`
-  type Query {
-    users: [User!]
-    me: User
-    user(id: ID!): User
-
-    messages: [Message!]!
-    message(id: ID!): Message!
-  }
-
-  type Mutation {
-    createMessage(text: String!): Message!
-    deleteMessage(id: ID!): Boolean!
-    updateMessage(text: String!, id: ID!): Boolean!
-  }
-
-  type User {
-    id: ID!
-    username: String!
-    messages: [Message!]
-  }
-
-  type Message {
-    id: ID!
-    text: String!
-    user: User!
-  }
-`;
-
-const resolvers = {
-  Query: {
-    users: () => {
-      return Object.values(users);
-    },
-    // parent, args, context, info
-    user: (parent, { id }) => {
-      return users[id];
-    },
-    me: (parent, args, { me }) => {
-      return me;
-    },
-    messages: () => {
-      return Object.values(messages);
-    },
-    message: (parent, { id }) => {
-      return messages[id];
-    },
-  },
-  Mutation: {
-    createMessage: (parent, { text }, { me }) => {
-      const id = uuidv4();
-      const message = {
-        id,
-        text,
-        userId: me.id,
-      };
-      messages[id] = message;
-      users[me.id].messageIds.push(id);
-
-      return message;
-    },
-    deleteMessage: (parent, { id }) => {
-      const { [id]: message, ...otherMessages } = messages;
-      if (!message) {
-        return false;
-      }
-      messages = otherMessages;
-      return true;
-    },
-    updateMessage: (parent, { text, id }) => {
-      const { [id]: message } = messages;
-      if (!message) {
-        return false;
-      }
-      message.text = text;
-      return true;
-    },
-  },
-  User: {
-    messages: (user) => {
-      return Object.values(messages).filter((message) => message.userId === user.id);
-    },
-  },
-  Message: {
-    user: (message) => {
-      return users[message.userId];
-    },
-  },
-};
-
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
   context: {
-    me: users[1],
+    models,
+    me: models.users[1],
   },
 });
 
