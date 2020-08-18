64a46d51316527afcb96f0d18d8a1465f64e903f
20twa07 <48910851+20twa07@users.noreply.github.com>
2020-08-18 15:12:27 +0200

    add deleteMessage mutation

--- src/index.js
+++ src/index.js
@@ -47,6 +47,7 @@ const schema = gql`
 
   type Mutation {
     createMessage(text: String!): Message!
+    deleteMessage(id: ID!): Boolean!
   }
 
   type User {
@@ -93,6 +94,14 @@ const resolvers = {
       users[me.id].messageIds.push(id);
 
       return message;
+    },
+    deleteMessage: (parent, { id }) => {
+      const { [id]: message, ...otherMessages } = messages;
+      if (!message) {
+        return false;
+      }
+      messages = otherMessages;
+      return true;
     },
   },
   User: {
