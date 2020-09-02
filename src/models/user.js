d01c388016f867edf3c203a9817fdf1a8f1fbdbb
20twa07 <48910851+20twa07@users.noreply.github.com>
2020-09-02 18:53:17 +0200

    Auth/SignIn -update models, schema & resolvers

--- src/models/user.js
+++ src/models/user.js
@@ -54,6 +54,9 @@ const user = (sequelize, DataTypes) => {
     const saltRounds = 10;
     return await bcrypt.hash(this.password, saltRounds);
   };
+  User.prototype.validatePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+  };
 
   return User;
 };
